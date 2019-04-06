5a9b2e962fbdfcad56b3e685cea66d106b229f1c
Patrick Schleizer <adrelanos@riseup.net>
2019-04-06 10:21:17 +0000

    sudo

--- usr/lib/python3/dist-packages/whonix_repository_wizard/whonix_repository_wizard.py
+++ usr/lib/python3/dist-packages/whonix_repository_wizard/whonix_repository_wizard.py
@@ -201,7 +201,7 @@ def main():
 
     # root check.
     if os.getuid() != 0:
-        print('ERROR: This must be run as root!\nUse "gksudo".')
+        print('ERROR: This must be run as root!\nUse "sudo --set-home".')
         not_root = gui_message(common.tr_file, 'not_root')
         sys.exit(1)
 
