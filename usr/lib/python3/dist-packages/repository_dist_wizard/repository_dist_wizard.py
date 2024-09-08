d04c7d507f051df80cfa03f27f288541ca717ba9
Patrick Schleizer <adrelanos@whonix.org>
2024-09-08 13:14:53 -0400

    root check

--- usr/lib/python3/dist-packages/repository_dist_wizard/repository_dist_wizard.py
+++ usr/lib/python3/dist-packages/repository_dist_wizard/repository_dist_wizard.py
@@ -223,6 +223,10 @@ def signal_handler(sig, frame):
     sys.exit(128 + sig)
 
 def main():
+    if os.geteuid() == 0:
+        print('repository_dist_wizard.py: ERROR: Do not run with sudo / as root!')
+        sys.exit(1)
+
     app = QApplication(sys.argv)
 
     signal.signal(signal.SIGINT, signal_handler)
